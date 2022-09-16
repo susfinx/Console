3ff1bb166b4301982ad2866fbf8f7df5a181cd96
Roman <79613377185R@gmail.com>
2022-09-16 10:50:20 +0300

    If Else Project

---
+++ If else Project/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("VVedite imya");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.Write("URA Eto MASHA!");
+}
+else 
+{
+    Console.Write("Privet, ");
+    Console.Write(username);
+}
