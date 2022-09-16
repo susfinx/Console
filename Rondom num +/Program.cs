047366a5049df18cca8065ce2e3182c5057f5c2f
Roman <79613377185R@gmail.com>
2022-09-16 10:28:48 +0300

    slojenie Rondom num

---
+++ Rondom num +/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,20);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
